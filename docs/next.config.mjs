83c7ec5ae119e7ef9ddf0447ab5cb97a36ce9d6e
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-18 22:04:43 +0700

    Refactor next.config.mjs for production settings

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'support-old-android-docs'
+
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true, 
-  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
 
 export default nextConfig
